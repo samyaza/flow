ac7e8909150e5b4d3e3eb9361516ca60d003eb7d
samyaza <fallenhunter777@gmail.com>
2013-10-02 22:05:06 -0400

    Fixedd up flow.h, needs to be tested

--- flow.h
+++ flow.h
@@ -8,48 +8,23 @@ class Node{
         Node* next;
         T data;
     public:
-        Node();
+        Node(){};
         Node(T,Node<T>*);
     template<class U> friend class FlowQueue;
-	template<class U> friend class FlowProducer;
-	template<class U> friend class FlowConsumer;
+    template<class U> friend class FlowProducer;
+    template<class U> friend class FlowConsumer;
 };
-template<class T> Node<T>::Node(){
-}
 template<class T> Node<T>::Node(T data, Node<T>* next){
     this->data = data;
     this->next = next;
-}
-template<class T>class FlowQueue{  
-    
-    private:
-        Node<T>* front;
-        Node<T>* back;
-        int _size;
-        T get();
-        void put(T data);
-    public:
-        FlowQueue();
-        ~FlowQueue();
-        bool isEmpty(){return (this->_size == 0)? true : false;}
-        int size(){return this->_size;}        
-};
-template<class T> FlowQueue<T>::FlowQueue(){
-    _size = 0;
-	front = new Node();
-	back = front;
-}
-template<class T> FlowQueue<T>::~FlowQueue(){
-    while(_size>=0)
-        this->get();
 }
 
 template<class T> class FlowConsumer{
     private:
         int* _size;
-		Node<T>* front;
+	Node<T>* front;
     public:
-         FlowProducer(int* size,Node<T>* queue_front):_size(size),back(queue_front);
+         FlowConsumer(int* size,Node<T>* queue_front):_size(size), front(queue_front){};
          T get();
 };
 
@@ -66,15 +41,41 @@ template<class T> T FlowConsumer<T>::get(){
 template<class T> class FlowProducer{
     private:
         int* _size;
-		Node<T>* back;
+	Node<T>* back;
     public:
-         FlowProducer(int* size,Node<T>* queue_back):_size(size),back(queue_back);
+         FlowProducer(int* size,Node<T>* queue_back):_size(size),back(queue_back){};
          void put(T data);
 };
 
 template<class T> void FlowProducer<T>::put(T data){
-	back->data = data;
-	back->next = new Node<T>();
-	back = back->next;
+    back->data = data;
+    back->next = new Node<T>();
+    back = back->next;
     *_size++;
+}
+template<class T>class FlowQueue{     
+    private:
+        Node<T>* front;
+        Node<T>* back;
+        int _size;
+    public:
+        FlowQueue();
+        ~FlowQueue();
+        bool isEmpty(){return (this->_size == 0)? true : false;}
+        int size(){return this->_size;}  
+        FlowProducer<T> get_producer(){return FlowProducer<T>(&_size,back);}
+        FlowConsumer<T> get_consumer(){return FlowConsumer<T>(&_size,front);}
+};
+template<class T> FlowQueue<T>::FlowQueue(){
+    _size = 0;
+    front = new Node<T>();
+    back = front;
+}
+template<class T> FlowQueue<T>::~FlowQueue(){
+    while(_size>=0){
+        Node<T>* temp = front;
+        front = front->next;
+        delete temp;
+        _size--;
+    }
 }
--- test.cpp
+++ test.cpp
@@ -6,24 +6,30 @@ using namespace std;
 void print_function(FlowQueue<string> *que,int id){
     string out;
     do{
-        out = que->get();
+       // out = que->get();
         cout << "From " << id << ": " << out << endl;
     }while(out != "exit");
 }
 
 int main(int argc, char** argv) {
-    
-    FlowQueue<string> test; 
-    thread t1(print_function, &test,1);
-    thread t2(print_function, &test,2);
-    string out;
-    do{
-        cin >> out;
-        test.put(out);
-    }while(out != "exit");
-    test.put(out);
-    t1.join();
-    t2.join();
+    cout << "TET";
+//    FlowQueue<string> test; 
+//    
+//    auto pro = test.get_producer();
+//    pro.put("Test");
+//    cout << "hee";
+//    auto con = test.get_consumer();
+//    cout << con.get() << endl;
+//    thread t1(print_function, &test,1);
+//    thread t2(print_function, &test,2);
+//    string out;
+//    do{
+//        cin >> out;
+//        test.put(out);
+//    }while(out != "exit");
+//    test.put(out);
+//    t1.join();
+//    t2.join();
     return 0;
 }
 
