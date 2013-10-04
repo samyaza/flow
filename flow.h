74fc91e1c0800256ceb564790d3a7fc8a58f6dd1
Chris Weist <cweist@keywcorp.com>
2013-10-04 08:45:22 -0400

    Added locks to queue, single test is working

--- flow.h
+++ flow.h
@@ -1,6 +1,4 @@
-#include <iostream>
-
-using namespace std;
+#include <mutex>
 
 template<class T>
 class Node{
@@ -22,19 +20,22 @@ template<class T> Node<T>::Node(T data, Node<T>* next){
 template<class T> class FlowConsumer{
     private:
         int* _size;
-	Node<T>** front;
+		Node<T>** front;
+		std::mutex* front_lock;
     public:
-         FlowConsumer(int* size,Node<T>** queue_front):_size(size), front(queue_front){};
+         FlowConsumer(int* size,Node<T>** queue_front,std::mutex* lock):_size(size), front(queue_front),front_lock(lock){};
          T get();
 };
 
 template<class T> T FlowConsumer<T>::get(){
+	front_lock->lock();
     while(*_size==0);
 	T ret = (*front)->data;
     Node<T>* temp = *front;	
-    *front = (*front)->next;	
-    delete temp;	
-    (*_size)--;
+    *front = (*front)->next;
+	(*_size)--;	
+	front_lock->unlock();
+    delete temp;	    
     return ret;
 }
 
@@ -42,29 +43,34 @@ template<class T> class FlowProducer{
     private:
         int* _size;
 		Node<T>** back;
+		std::mutex* back_lock;
     public:
-         FlowProducer(int* size,Node<T>** queue_back):_size(size),back(queue_back){};
+         FlowProducer(int* size,Node<T>** queue_back,std::mutex* lock):_size(size),back(queue_back),back_lock(lock){};
          void put(T data);
 };
 
 template<class T> void FlowProducer<T>::put(T data){
+	back_lock->lock();
     (*back)->data = data;
     (*back)->next = new Node<T>();
     *back = (*back)->next;	
     (*_size)++;	
+	back_lock->unlock();
 }
 template<class T>class FlowQueue{     
     private:
         Node<T>* front;
         Node<T>* back;
         int _size;
+		std::mutex front_lock;
+		std::mutex back_lock;
     public:
         FlowQueue();
         ~FlowQueue();
         bool isEmpty(){return (this->_size == 0)? true : false;}
         int size(){return this->_size;}  
-        FlowProducer<T> get_producer(){return FlowProducer<T>(&_size,&back);}
-        FlowConsumer<T> get_consumer(){return FlowConsumer<T>(&_size,&front);}
+        FlowProducer<T> get_producer(){return FlowProducer<T>(&_size,&back,&back_lock);}
+        FlowConsumer<T> get_consumer(){return FlowConsumer<T>(&_size,&front,&front_lock);}
 };
 template<class T> FlowQueue<T>::FlowQueue(){
     _size = 0;
--- test.cpp
+++ test.cpp
@@ -3,10 +3,10 @@
 #include "flow.h"
 using namespace std;
 
-void print_function(FlowQueue<string> *que,int id){
+void print_function(FlowConsumer<string> que,int id){
     string out;
     do{
-       // out = que->get();
+        out = que.get();
         cout << "From " << id << ": " << out << endl;
     }while(out != "exit");
 }
@@ -14,24 +14,24 @@ void print_function(FlowQueue<string> *que,int id){
 int main(int argc, char** argv) {
     FlowQueue<string> test; 
 //    
-    auto pro = test.get_producer();
-     pro.put("Test");
-	 pro.put("NEW");
-     auto con = test.get_consumer();
-     cout << con.get() << endl;
-	cout << con.get() << endl;
-	pro.put("Test");
+     auto pro = test.get_producer();
+     // pro.put("Test");
+	 // pro.put("NEW");
+ //     auto con = test.get_consumer();
+     // cout << con.get() << endl;
+	// cout << con.get() << endl;
+	// pro.put("Test");
 	
 
-//    thread t1(print_function, &test,1);
-//    thread t2(print_function, &test,2);
-//    string out;
-//    do{
-//        cin >> out;
-//        test.put(out);
-//    }while(out != "exit");
-//    test.put(out);
-//    t1.join();
+   thread t1(print_function, test.get_consumer(),1);
+//   thread t2(print_function, &test,2);
+   string out;
+   do{
+       cin >> out;
+       pro.put(out);
+   }while(out != "exit");
+   pro.put(out);
+   t1.join();
 //    t2.join();
     return 0;
 }
