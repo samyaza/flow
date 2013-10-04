6b06120cc8153a93d5bb988449a64b49cf5fff4f
Chris Weist <cweist@keywcorp.com>
2013-10-04 09:58:55 -0400

    Two Consumers test working

--- test.cpp
+++ test.cpp
@@ -12,27 +12,19 @@ void print_function(FlowConsumer<string> que,int id){
 }
 
 int main(int argc, char** argv) {
-    FlowQueue<string> test; 
-//    
-     auto pro = test.get_producer();
-     // pro.put("Test");
-	 // pro.put("NEW");
- //     auto con = test.get_consumer();
-     // cout << con.get() << endl;
-	// cout << con.get() << endl;
-	// pro.put("Test");
 	
-
-   thread t1(print_function, test.get_consumer(),1);
-//   thread t2(print_function, &test,2);
-   string out;
-   do{
-       cin >> out;
-       pro.put(out);
-   }while(out != "exit");
-   pro.put(out);
-   t1.join();
-//    t2.join();
-    return 0;
+    FlowQueue<string> test;     
+    auto pro = test.get_producer();
+	thread t1(print_function, test.get_consumer(),1);
+	thread t2(print_function, test.get_consumer(),2);
+	string out;
+	do{
+	   cin >> out;
+	   pro.put(out);
+	}while(out != "exit");
+	pro.put(out);
+	t1.join();
+	t2.join();
+	return 0;
 }
 
